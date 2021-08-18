8341692c7d1ec5458dc43bff09578f94fec6fe13
hoangnhutvu <vu23071997@gmail.com>
2021-08-18 11:57:24 +0700

    Refactor page - load new example json

--- src/views/Dashboard/IndexerCreate.vue
+++ src/views/Dashboard/IndexerCreate.vue
@@ -23,40 +23,98 @@
     </base-header>
     <b-container fluid class="mt-6">
       <b-row>
-        <!-- <b-col cols="9">
-          <base-input
-            label="Indexer Name"
-            rules="required"
-            name="Indexer Name"
-            placeholder="Fill Your Indexer Name "
-            v-model="table"
-          >
-          </base-input>
-        </b-col> -->
         <b-col cols="9">
+          <!-- <h1>
+            Is WASM Project
+            <base-switch
+              class="mr-2"
+              style="font-size: 24px; "
+              type="success"
+              v-model="isWasmFile"
+            ></base-switch>
+          </h1> -->
           <Tabs :centered="false">
-            <Tab ref="project" title="project.yaml">
+            <Tab ref="schema" title="abis">
               <div class="cover-editor mt-3">
-                <prism-editor
-                  class="my-editor"
-                  v-model="project"
-                  :highlight="highlighter"
-                  line-numbers
-                  :readonly="isOnCompile"
-                ></prism-editor>
+                <Tabs :centered="true" type="danger">
+                  <Tab
+                    v-for="item in abisFiles"
+                    :key="item.name"
+                    :title="item.name"
+                    type="danger"
+                  >
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                        v-model="item.data"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                </Tabs>
               </div>
             </Tab>
-            <!-- <Tab ref="lib" title="lib.rs">
+            <Tab ref="configs" title="configs">
               <div class="cover-editor mt-3">
-                <prism-editor
-                  class="my-editor"
-                  v-model="lib"
-                  :highlight="highlighter"
-                  line-numbers
-                ></prism-editor>
+                <!-- <Tabs :centered="true">
+                  <Tab ref="project" title="project.yaml">
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        v-model="project"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                  <Tab ref="schema" title="schema.graphql">
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        v-model="schema"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                  <Tab ref="package" title="package.json">
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        v-model="packagejs"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                </Tabs> -->
+
+                <Tabs :centered="true" type="danger">
+                  <Tab
+                    v-for="item in configFiles"
+                    :key="item.name"
+                    :title="item.name"
+                    type="danger"
+                  >
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                        v-model="item.data"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                </Tabs>
               </div>
-            </Tab> -->
-            <Tab ref="mapping" title="mapping.rs">
+            </Tab>
+            <!-- <Tab v-if="!isWasmFile" ref="mapping" title="mapping.rs">
               <div class="cover-editor mt-3">
                 <prism-editor
                   class="my-editor"
@@ -66,37 +124,26 @@
                   :readonly="isOnCompile"
                 ></prism-editor>
               </div>
-            </Tab>
-            <!-- <Tab ref="models" title="models.rs">
-              <div class="cover-editor mt-3">
-                <prism-editor
-                  class="my-editor"
-                  v-model="models"
-                  :highlight="highlighter"
-                  line-numbers
-                ></prism-editor>
-              </div>
             </Tab> -->
-            <Tab ref="schema" title="schema.graphql">
+            <Tab ref="schema" title="mappings">
               <div class="cover-editor mt-3">
-                <prism-editor
-                  class="my-editor"
-                  v-model="schema"
-                  :highlight="highlighter"
-                  line-numbers
-                  :readonly="isOnCompile"
-                ></prism-editor>
-              </div>
-            </Tab>
-            <Tab ref="schema" title="mapping.ts">
-              <div class="cover-editor mt-3">
-                <prism-editor
-                  class="my-editor"
-                  v-model="mappingts"
-                  :highlight="highlighter"
-                  line-numbers
-                  :readonly="isOnCompile"
-                ></prism-editor>
+                <Tabs :centered="true">
+                  <Tab
+                    v-for="item in mappingsFiles"
+                    :key="item.name"
+                    :title="item.name"
+                  >
+                    <div class="cover-editor mt-3">
+                      <prism-editor
+                        class="my-editor"
+                        :highlight="highlighter"
+                        line-numbers
+                        :readonly="isOnCompile"
+                        v-model="item.data"
+                      ></prism-editor>
+                    </div>
+                  </Tab>
+                </Tabs>
               </div>
             </Tab>
           </Tabs>
@@ -178,6 +225,7 @@ import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+import { json } from "d3";
 
 export default {
   components: {
@@ -200,19 +248,27 @@ export default {
     }
   },
   mounted() {
-    if (this.chain == localStorage.getItem(LOCAL_STORE.CHAIN)) {
-      this.mapping = localStorage.getItem(LOCAL_STORE.MAPPING);
-      this.mappingts = localStorage.getItem(LOCAL_STORE.MAPPINGTS);
-      this.models = localStorage.getItem(LOCAL_STORE.MODELS);
-      this.project = localStorage.getItem(LOCAL_STORE.PROJECT);
-      this.lib = localStorage.getItem(LOCAL_STORE.LIB);
-      this.schema = localStorage.getItem(LOCAL_STORE.SCHEMA);
-      this.table = localStorage.getItem(LOCAL_STORE.TABLE);
-      this.compilation_id = localStorage.getItem(LOCAL_STORE.COMPILE);
-    }
-
     this.loadDefaultSchema();
     localStorage.setItem(LOCAL_STORE.CHAIN, this.chain);
+    if (this.chain == localStorage.getItem(LOCAL_STORE.CHAIN)) {
+      // this.mapping = localStorage.getItem(LOCAL_STORE.MAPPING);
+      // this.mappingsFiles = localStorage.getItem(LOCAL_STORE.mappingsFiles);
+      // this.models = localStorage.getItem(LOCAL_STORE.MODELS);
+      // this.project = localStorage.getItem(LOCAL_STORE.PROJECT);
+      // this.lib = localStorage.getItem(LOCAL_STORE.LIB);
+      // this.schema = localStorage.getItem(LOCAL_STORE.SCHEMA);
+      // this.table = localStorage.getItem(LOCAL_STORE.TABLE);
+      // this.compilation_id = localStorage.getItem(LOCAL_STORE.COMPILE);
+      this.mappingsFiles = JSON.parse(
+        localStorage.getItem(LOCAL_STORE.MAPPING)
+      );
+      this.configFiles = JSON.parse(localStorage.getItem(LOCAL_STORE.MODELS));
+      this.abisFiles = JSON.parse(localStorage.getItem(LOCAL_STORE.LIB));
+    } else {
+      this.configFiles = [];
+      this.mappingsFiles = [];
+      this.abisFiles = [];
+    }
   },
   beforeUpdate() {
     this.catchData();
@@ -225,14 +281,19 @@ export default {
       currentPage: 1,
       is: 1,
       templates: [],
+      configFiles: [],
+      mappingsFiles: [],
       compileLog: "",
       compilation_id: "",
       mapping: "",
+      packagejs: "",
+      abisFiles: "",
       models: "",
       schema: DEFAULT_SCHEMA,
       project: "",
       lib: "",
       table: "",
+      isWasmFile: true,
       isShowHasura: false,
       isOnCompile: false
     };
@@ -282,21 +343,29 @@ export default {
     async loadDefaultSchema() {
       this.$loading(true);
       try {
+        this.templates = [];
         this.templates = await Request(`https://raw.githubusercontent.com`)
           .get(
             `https://raw.githubusercontent.com/massbitprotocol/massbitprotocol/main/user-example/examples.json`
           )
           .then(res => {
-            var data = res.data[this.chain];
-            var result = [];
-            if (data) {
-              var listKey = Object.keys(data);
-              for (let index = 0; index < listKey.length; index++) {
-                const element = listKey[index];
-                result.push({ name: element, data: data[element] });
+            var result = res.data[this.chain];
+
+            var data = [];
+            if (result) {
+              if (!result.so && !result.wasm) {
+                return this.convertObjToArray(result);
+              }
+              if (result.so) {
+                data = data.concat(this.convertObjToArray(result.so));
+              }
+
+              if (result.wasm) {
+                data = data.concat(this.convertObjToArray(result.wasm));
               }
             }
-            return result;
+
+            return data;
           })
           .catch(handleError);
       } catch (error) {
@@ -304,8 +373,32 @@ export default {
           text: error
         });
       }
-
       this.$loading(false);
+    },
+    convertObjToArray(data) {
+      var result = [];
+      if (data) {
+        var listKey = Object.keys(data);
+        for (let index = 0; index < listKey.length; index++) {
+          const element = listKey[index];
+          result.push({ name: element, data: data[element] });
+        }
+      }
+      return result;
+    },
+    convertObjToArraString(data) {
+      var result = [];
+      if (data) {
+        var listKey = Object.keys(data);
+        for (let index = 0; index < listKey.length; index++) {
+          const element = listKey[index];
+          result.push({
+            name: element,
+            data: decodeURIComponent(data[element])
+          });
+        }
+      }
+      return result;
     },
     async loadRawDataGit(url) {
       if (url && url.length > 1) {
@@ -320,25 +413,50 @@ export default {
       }
     },
     async loadTemplate(data) {
+      console.log(data);
       this.$loading(true);
-      this.mapping = await this.loadRawDataGit(data["mapping.rs"] || "");
-      this.mappingts = await this.loadRawDataGit(data["mapping.ts"] || "");
-      this.models = await this.loadRawDataGit(data["models.rs"] || "");
-      this.lib = await this.loadRawDataGit(data["lib.rs"] || "");
-      this.project = await this.loadRawDataGit(data["project.yaml"] || "");
-      this.schema = await this.loadRawDataGit(data["schema.graphql"] || "");
+      if (data) {
+        this.mappingsFiles = this.convertObjToArraString(data["src"]);
+        this.configFiles = this.convertObjToArraString(data["configs"]);
+        this.abisFiles = this.convertObjToArraString(data["abis"]);
+
+        // this.mappingsFiles = this.convertObjToArray(data["src"]);
+        // this.configFiles = this.convertObjToArray(data["configs"]);
+        // this.abisFiles = this.convertObjToArray(data["abis"]);
+        this.isWasmFile = data["isWasmFile"] ? data["isWasmFile"] : false;
+      }
+
+      // this.mapping = await this.loadRawDataGit(data["mapping.rs"] || "");
+      // this.abisFiles = await this.loadRawDataGit(data["abis"] || "");
+      // this.models = await this.loadRawDataGit(data["models.rs"] || "");
+      // this.lib = await this.loadRawDataGit(data["lib.rs"] || "");
+      // this.project = await this.loadRawDataGit(data["project.yaml"] || "");
+      // this.schema = await this.loadRawDataGit(data["schema.graphql"] || "");
+
       this.compilation_id = "";
       this.$loading(false);
     },
     catchData() {
       localStorage.setItem(LOCAL_STORE.MAPPING, this.mapping);
-      localStorage.setItem(LOCAL_STORE.MAPPINGTS, this.mappingts);
-      localStorage.setItem(LOCAL_STORE.MODELS, this.models);
-      localStorage.setItem(LOCAL_STORE.PROJECT, this.project);
-      localStorage.setItem(LOCAL_STORE.LIB, this.lib);
-      localStorage.setItem(LOCAL_STORE.TABLE, this.table);
-      localStorage.setItem(LOCAL_STORE.COMPILE, this.compilation_id);
-      localStorage.setItem(LOCAL_STORE.SCHEMA, this.schema);
+      localStorage.setItem(
+        LOCAL_STORE.MAPPING,
+        JSON.stringify(this.mappingsFiles)
+      );
+      localStorage.setItem(
+        LOCAL_STORE.MODELS,
+        JSON.stringify(this.configFiles)
+      );
+      localStorage.setItem(LOCAL_STORE.LIB, JSON.stringify(this.abisFiles));
+      // localStorage.setItem(LOCAL_STORE.LIB, null);
+      // localStorage.setItem(LOCAL_STORE.MAPPING, null);
+      // localStorage.setItem(LOCAL_STORE.MODELS, null);
+
+      // localStorage.setItem(LOCAL_STORE.MODELS, this.models);
+      // localStorage.setItem(LOCAL_STORE.PROJECT, this.project);
+      // localStorage.setItem(LOCAL_STORE.LIB, this.lib);
+      // localStorage.setItem(LOCAL_STORE.TABLE, this.table);
+      // localStorage.setItem(LOCAL_STORE.COMPILE, this.compilation_id);
+      // localStorage.setItem(LOCAL_STORE.SCHEMA, this.schema);
     },
     isByteArray(array) {
       if (array && array.length > 0) return true;
@@ -349,23 +467,46 @@ export default {
     },
     async onProcess(action) {
       // this.$loading(true);
+
       this.isOnCompile = true;
 
-      this.isShowHasura = false;
-
       try {
+        this.isShowHasura = false;
+        var requestObj = {
+          mappings: {},
+          abis: {},
+          configs: {},
+          compilation_id: this.compilation_id
+        };
+
+        if (action == "compile") {
+          console.log(this.isWasmFile);
+          action = this.isWasmFile == true ? "compile/wasm" : "compile/so";
+
+          console.log(action);
+          if (this.mappingsFiles) {
+            for (let index = 0; index < this.mappingsFiles.length; index++) {
+              const element = this.mappingsFiles[index];
+              requestObj.mappings[element.name] = element.data;
+            }
+          }
+
+          if (this.abisFiles) {
+            for (let index = 0; index < this.abisFiles.length; index++) {
+              const element = this.abisFiles[index];
+              requestObj.abis[element.name] = element.data;
+            }
+          }
+
+          if (this.configFiles) {
+            for (let index = 0; index < this.configFiles.length; index++) {
+              const element = this.configFiles[index];
+              requestObj.configs[element.name] = element.data;
+            }
+          }
+        }
         await Request()
-          .post(`/${action}`, {
-            "mapping.rs": this.covertToURL(this.mapping),
-            "mapping.ts": this.covertToURL(this.mappingts),
-            // "models.rs": this.covertToURL(this.models),
-            "project.yaml": this.covertToURL(this.project),
-            "schema.graphql": this.covertToURL(this.schema),
-            // "lib.rs": this.covertToURL(this.lib),
-            // table: this.table,
-            compilation_id: this.compilation_id
-            // network_type: this.chain
-          })
+          .post(`/${action}`, requestObj)
           .then(async res => {
             if (res.data.payload && res.data.payload.length > 0) {
               this.compilation_id = res.data.payload;
@@ -483,6 +624,6 @@ export default {
 .cover-editor {
   background: #2d2d2d;
   height: 400px;
-  padding: 10px;
+  padding: 10px 0px 50px 0px;
 }
 </style>
