693a7dd2a7d7bf4c68bcd4c4c5651b69359c6b26
hoangnhutvu <vu23071997@gmail.com>
2021-08-19 18:46:33 +0700

    Fix for eth

--- src/views/Dashboard/DashboardTable/IndexersTable.vue
+++ src/views/Dashboard/DashboardTable/IndexersTable.vue
@@ -122,10 +122,11 @@ export default {
           var result = res.data.result;
           if (result && result.length > 0) {
             this.indexersList = result;
-            this.indexersList = this.indexersList.filter(
-              x =>
-                x.network.toString().toLowerCase() ==
-                this.indexerName.toLowerCase()
+            this.indexersList = this.indexersList.filter(x =>
+              x.network
+                .toString()
+                .toLowerCase()
+                .includes(this.indexerName.toLowerCase())
             );
           }
 
